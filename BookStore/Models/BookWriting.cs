9422c938a6b6bd2f69ce2b64f17f65b9057343be
tranviendai <viendai50@gmail.com>
2023-03-05 18:13:05 +0700

    Controller/Views
    Controller + Views "P1"

---
+++ BookStore/Models/BookWriting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class BookWriting
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int index { get; set; }
+
+        public int authorID { get; set; }
+        [ForeignKey("authorID")]
+        public Author author { get; set; }
+
+        public int bookID { get; set; }
+        [ForeignKey("bookID")]
+        public Book book { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage ="Vui lòng nhập vai trò")]
+        [Display(Name = "Vai Trò")]
+        public string role { get; set; }
+        
+    }
+}
